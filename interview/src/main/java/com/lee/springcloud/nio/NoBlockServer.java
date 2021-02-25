aa172c409331faa0238c1657954b2c336858be32
leevanes <leevanes@163.com>
2021-02-25 17:31:30 +0800

    测试基本的反射代码

--- interview/src/main/java/com/lee/springcloud/nio/NoBlockServer.java
+++ interview/src/main/java/com/lee/springcloud/nio/NoBlockServer.java
@@ -22,44 +22,29 @@ public class NoBlockServer {
 
         server.register(selector, SelectionKey.OP_ACCEPT);
 
+        int select = selector.select();
+
         while (selector.select() > 0) {
-
             Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-
             while (iterator.hasNext()) {
-
                 SelectionKey selectionKey = iterator.next();
-
                 if (selectionKey.isAcceptable()) {
-
                     SocketChannel client = server.accept();
-
                     client.configureBlocking(false);
-
                     client.register(selector, SelectionKey.OP_READ);
-
                 } else if (selectionKey.isReadable()) {
                     SocketChannel client = (SocketChannel) selectionKey.channel();
-
                     ByteBuffer buffer = ByteBuffer.allocate(1024);
-
                     FileChannel outChannel = FileChannel.open(Paths.get("2.png"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
-
                     while (client.read(buffer) > 0) {
                         buffer.flip();
-
                         outChannel.write(buffer);
-
                         buffer.clear();
                     }
-
                     ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
-
                     writeBuffer.put("the img is received".getBytes(StandardCharsets.UTF_8));
                     writeBuffer.flip();
                     client.write(writeBuffer);
-
-
                 }
                 iterator.remove();
             }
